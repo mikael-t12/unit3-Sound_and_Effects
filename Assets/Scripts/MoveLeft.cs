f176489cd0fab604e6736e9b3bee578ff0ddc495
Mikael <cmito004@edu.xamk.fi>
2020-11-03 09:39:20 +0200

    Add that if the game is over, don't spawn move obstacles anymore

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 30;
+
+    //Reference to the PlayerController Script
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        //If the game is over, stop moving.
+        if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
     }
 }
