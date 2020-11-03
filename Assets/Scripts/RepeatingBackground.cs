b3bf321689c21a3e9505c04bcb6b9761f6d6e05f
Mikael <cmito004@edu.xamk.fi>
2020-11-03 09:28:37 +0200

    Finish repeating background

--- Assets/Scripts/RepeatingBackground.cs
+++ Assets/Scripts/RepeatingBackground.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class RepeatingBackground : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
+        //Set the starting position
         startPos = transform.position;
+        //Get the box collier from the component
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < startPos.x - 50)
+        //If the background's position reaches a the repeatWidth, reset it.
+        if(transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
